f59eff2635ac0a1f9fa8b56a07f915fa894632ab
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2018-02-27 08:37:00 +0100

    update deps

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :distribution :repo}
   :url "http://github.com/threatgrid/ring-jwt-middleware"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [yogsototh/clj-jwt "0.2.0"]
+                 [yogsototh/clj-jwt "0.2.1"]
                  [threatgrid/clj-momo "0.2.9"]
                  [org.clojure/tools.logging "0.3.1"]
                  [metosin/ring-http-response "0.8.2"]
