f347cdf19022fbb8014134b429e4a382a183b6a9
Yann Esposito (Yogsototh) <yann.esposito@gmail.com>
2021-07-19 17:37:09 +0200

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ring-jwt-middleware "1.0.2"
+(defproject threatgrid/ring-jwt-middleware "1.0.3-SNAPSHOT"
   :description "A simple middleware to deal with JWT Authentication"
   :pedantic? :abort
   :license {:name "Eclipse Public License - v 1.0"
